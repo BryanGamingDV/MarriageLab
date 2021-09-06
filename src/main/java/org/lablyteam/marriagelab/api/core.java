e813c510ac7034f2750cdbe2f56b3719e30d925e
Martin <78501668+FhatersDev@users.noreply.github.com>
2021-09-05 22:33:31 -0500

    Add files via upload

---
+++ src/main/java/org/lablyteam/marriagelab/api/core.java
@@ -0,0 +1,6 @@
+package org.lablyteam.marriagelab.api;
+
+public interface core {
+
+    void init();
+}
---
+++ src/main/java/org/lablyteam/marriagelab/builders/TextUtils.java
@@ -0,0 +1,13 @@
+package org.lablyteam.marriagelab.builders;
+
+import org.bukkit.ChatColor;
+
+public class TextUtils {
+
+    public String colorize(String text){
+
+        return ChatColor.translateAlternateColorCodes('&',text);
+    }
+
+
+}
